878fdb9e47a3c23ac50efbe01eff7f076d6db6dd
iResNovae <iresnovaeserg@agmail.com>
2013-11-17 14:27:56 +0300

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ads::Application.config.secret_token = '6394a8bb7b7ec185e8d65ed4fdd4527948484d978eb901bfac9a16b8b99f7d5b3b20b3f9f156db40bdc8fc5714b2f9d4235c3951a35f6a06920acad439115ad0'
